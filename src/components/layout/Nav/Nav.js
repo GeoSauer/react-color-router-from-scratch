b7e138c7f9bd640c7fa9c7db5713255479cefbcc
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-24 16:51:27 -0700

    refactored out a ton of redundant css

--- src/components/layout/Nav/Nav.js
+++ src/components/layout/Nav/Nav.js
@@ -5,38 +5,24 @@ import './Nav.css';
 export default function Nav() {
   return (
     <header className="header">
-      <ul>
-        <li>
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="red" to="/rgb/236/11/67">
-            Red Crayola
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="violet" to="/rgb/88/53/94">
-            English Violet
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="aquamarine " to="/rgb/122/231/199">
-            Aquamarine
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="green" to="/rgb/214/255/183">
-            Tea Green
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="canary" to="/rgb/255/246/137">
-            Canary
-          </NavLink>
-        </li>
-      </ul>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink className="red" to="/rgb/236/11/67">
+        Red Crayola
+      </NavLink>
+      <NavLink className="violet" to="/rgb/88/53/94">
+        English Violet
+      </NavLink>
+      <NavLink className="aquamarine " to="/rgb/122/231/199">
+        Aquamarine
+      </NavLink>
+      <NavLink className="green" to="/rgb/214/255/183">
+        Tea Green
+      </NavLink>
+      <NavLink className="canary" to="/rgb/255/246/137">
+        Canary
+      </NavLink>
     </header>
   );
 }
